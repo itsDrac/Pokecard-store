04019fb88875fca3158fccb6280b9d12db4ce29d
Sahaj <gpt.sahaj28@gmail.com>
2022-02-18 15:04:31 +0530

    Added app, run and config

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=app:creat_app
+FLASK_DEBUG=1
---
+++ app/config.py
@@ -0,0 +1,6 @@
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+MONGODB_HOST = os.environ.get('MONGODB_HOST')
+MONGODB_DB = os.environ.get('MONGODB_DB')
+MONGODB_USERNAME = os.environ.get('MONGODB_USERNAME')
